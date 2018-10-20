5ab3a28fb06b7a9457ffe831a85d11fb2e69cc77
SlimeBolt <dion.ooms@live.nl>
2018-10-20 23:07:05 +0200

    Created data percistency

---
+++ DnD-APP/Assets/Scripts/Rarity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+public class Rarity : SaveableObject {
+
+    new public string name = "";
+
+    public string description = "";
+
+    private void Start()
+    {
+        // This is testcode to test the id system and file system
+        SaveObject();
+        LoadObject();
+    }
+
+    private void Awake()
+    {
+        DataLocation = "/Rarity/";
+    }
+
+    public override void SaveObject()
+    {
+        RarityObject rarityObject = new RarityObject();
+        rarityObject.identifier = identifier;
+        rarityObject.name = name;
+        rarityObject.description = description;
+
+        Save(rarityObject);
+    }
+
+    public override void LoadObject()
+    {
+        object loadedObject = Load();
+
+        if (loadedObject != null)
+        {
+            RarityObject rarityObject = (RarityObject)loadedObject;
+            name = rarityObject.name;
+            description = rarityObject.description;
+        }
+    }
+}
+
+[Serializable]
+class RarityObject
+{
+    public int identifier;
+    public string name;
+    public string description;
+}
---
+++ DnD-APP/Assets/Scripts/SaveableObject.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public abstract class SaveableObject : MonoBehaviour {
+
+    protected string DataLocation = "/";
+
+    protected int identifier = -1;
+
+	protected  void Save(object serializableObject)
+    {
+        Directory.CreateDirectory(Application.persistentDataPath + DataLocation);
+
+        if (!File.Exists(Application.persistentDataPath + DataLocation + identifier + ".dat") || identifier == -1)
+        {
+            identifier = GetIdentifier();
+        }
+
+        Debug.Log(Application.persistentDataPath + DataLocation + identifier + ".dat");
+
+        BinaryFormatter formatter = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + DataLocation + identifier + ".dat");
+
+        formatter.Serialize(file, serializableObject);
+        file.Close();
+    }
+
+    protected object Load()
+    {
+        if(File.Exists( Application.persistentDataPath + DataLocation + identifier + ".dat" ))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + DataLocation + identifier + ".dat", FileMode.Open);
+            object data = formatter.Deserialize(file);
+            file.Close();
+
+            return data;
+        }
+        return null;
+    }
+
+    protected int GetIdentifier()
+    {
+        try
+        {
+            string file = Directory.GetFiles(Application.persistentDataPath + DataLocation)
+            .Select(filename => Path.GetFileNameWithoutExtension(filename))
+            .OrderBy(f => f)
+            .Max();
+            int.TryParse(file, out identifier);
+            identifier++;
+        }
+        catch (DirectoryNotFoundException e)
+        {
+            Debug.Log(e);
+            identifier = 0;
+        }
+        return identifier;
+    }
+
+    public abstract void SaveObject();
+    public abstract void LoadObject();
+}
